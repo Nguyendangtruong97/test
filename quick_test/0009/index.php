e4f6f78a52f12d195a69649e06973373c9c45118
USER <nguyendangtruong300397@gmail.com>
2019-01-14 14:01:16 +0700

    eqwe

---
+++ quick_test/0009/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h1>1 . Viết các câu lệnh php in ra các biến xuất hiện trên URL
+    sử dụng $_REQUEST và $_GET
+    URL giả định : index.php?controller=post&action=edit&id=100&return_page=abc.php
+</h1>
+<?php
+$controller = isset($_REQUEST["controller"]) ? (int) $_REQUEST["controller"] : '';
+$action = isset($_REQUEST["action"]) ? (int) $_REQUEST["action"] : '';
+$id = isset($_REQUEST["id"]) ? (int) $_REQUEST["id"] : '';
+$return_page = isset($_REQUEST["return_page"]) ? (int) $_REQUEST["return_page"] : '';
+if(isset($_REQUEST["controller"]) && isset($_REQUEST["action"]) && isset($_REQUEST["id"]) && isset($_REQUEST["return_page"])){
+    echo '<br>';
+    echo '<br>';
+    echo '<br>';
+    echo $_REQUEST['controller'];
+    echo '<br>';
+    echo $_REQUEST['action'];
+    echo '<br>';
+    echo $_REQUEST['id'];
+    echo '<br>';
+    echo $_REQUEST['return_page'];
+    echo '<br>';
+}
+?>
+<form action="" method="get">
+    <input type="text" name="controller" value="post">
+    <input type="text" name="action" value="edit">
+    <input type="text" name="id" value="1000">
+    <input type="text" name="return_page" value="XYZ.php">
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+
+</body>
+</html>
---
+++ quick_test/0011/index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style type="text/css">
+        table th{
+            border-left: 1px solid black;
+            border-bottom: 1px solid black;
+            width: 150px;
+        }
+        table td{
+            border-left: 1px solid black;
+            text-align: center;
+            border-bottom: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+<div>1 . Cho bạn 1 mảng
+    $student = array(); <br>
+    $student['01'] = array('name' => 'Nguyễn văn anh', 'age' => 21, 'point' => 7); <br>
+    $student['02'] = array('name' => 'Nguyễn thi hoa', 'age' => 23, 'point' => 8); <br>
+    $student['03'] = array('name' => 'Nguyễn văn tuấn', 'age' => 24, 'point' => 9); <br>
+    $student['04'] = array('name' => 'Lê văn thuận', 'age' => 25, 'point' => 7); <br>
+    $student['05'] = array('name' => 'Trịnh quang anh', 'age' => 26, 'point' => 1); <br>
+    $student['06'] = array('name' => 'Lê thuỳ linh', 'age' => 28, 'point' => 3); <br>
+    $student['07'] = array('name' => 'Lê thuỳ trang', 'age' => 25, 'point' => 5); <br>
+    $student['08'] = array('name' => 'Vũ đình hùng', 'age' => 20, 'point' => 8); <br>
+    $student['09'] = array('name' => 'Lê văn an', 'age' => 19, 'point' => 9); <br>
+
+    Hãy dùng dữ liệu sau và vòng lặp foreach để in ra 1 table thông tin của các sinh viên này
+    và xếp loại của họ dự tên số điểm mà họ có
+</div>
+<br>
+
+<?php
+$student = array();
+$student[] = array('name' => 'Nguyễn Đăng Trường', 'age' => 21, 'point' => 7);
+$student[] = array('name' => 'Nguyễn ngọc anh', 'age' => 23, 'point' => 8);
+$student[] = array('name' => 'Nguyễn hương', 'age' => 24, 'point' => 9);
+$student[] = array('name' => 'lương ngọc', 'age' => 25, 'point' => 7);
+$student[] = array('name' => ' quang anh', 'age' => 26, 'point' => 1);
+$student[] = array('name' => ' thuỳ linh', 'age' => 28, 'point' => 3);
+$student[] = array('name' => ' thuỳ trang', 'age' => 25, 'point' => 5);
+$student[] = array('name' => ' đình hùng', 'age' => 20, 'point' => 8);
+$student[] = array('name' => ' an định', 'age' => 19, 'point' => 9);
+$XL = array();
+foreach ($student as $key => $value){
+    array_push($student[$key], "XepLoai");
+    if($value['point']>=8){
+        $XL[$key] = 'Giỏi';
+    }elseif (6 <= $value['point'] && $value['point'] < 8){
+        $XL[$key] = 'Khá';
+    }else{
+        $XL[$key] = 'Trung Bình';
+    }
+}
+?>
+
+<table style="border: 1px solid black">
+    <thead>
+    <th>Họ Và Tên</th>
+    <th>Tuổi</th>
+    <th>Điểm Số</th>
+    <th>Xếp Loại</th>
+    </thead>
+    <?php
+    foreach ($student as $key => $value){
+        echo '<tbody>';
+        echo '<td>';
+        echo $value['name'];
+        echo '</td>';
+        echo '<td>';
+        echo $value['age'];
+        echo '</td>';
+        echo '<td>';
+        echo $value['point'];
+        echo '</td>';
+        echo '<td>';
+        echo $XL[$key];
+        echo '</td>';
+        echo '</tbody>';
+    }
+    ?>
+</table>
+
+</body>
+</html>
